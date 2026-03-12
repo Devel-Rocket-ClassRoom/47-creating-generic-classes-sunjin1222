f4f864573d7031e62ec773a2a84d7738666dc005
kim sunjin <rlatjswls14@gmail.com>
2026-03-12 18:28:32 +0900

    제출

--- GenericConverter/Program.cs
+++ GenericConverter/Program.cs
@@ -1,4 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
 Console.WriteLine("코드를 작성하세요.");
+
+
+class Converter<TInput, TOutput>
+{
+    private Func<TInput, TOutput> _convertFunc;
+
+
+    public Converter(Func<TInput, TOutput> input)
+    {
+        _convertFunc=input;
+    }
+
+
+    public TOutput Convert(TInput input)
+    {
+        if (input.TryGetValue())
+        { 
+        
+        }
+
+        return
+
+    }
+    public TOutput[] ConvertAll(TInput[] inputs)
+    {
+        return
+    }
+
+}
